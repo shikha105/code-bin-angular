d72850233539355478467ed3be8cab2f5076dc8e
shikha105 <shikha105789@gmail.com>
2024-07-14 21:07:32 -0700

    using the get snippet by id service and showing in html and applied css too

--- src/app/components/view-snippet/view-snippet.component.ts
+++ src/app/components/view-snippet/view-snippet.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from '../../services/database.service';
+import { Snippet } from '../../../models/snippet';
+import { ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-view-snippet',
   standalone: true,
   imports: [],
   templateUrl: './view-snippet.component.html',
-  styleUrl: './view-snippet.component.css'
+  styleUrl: './view-snippet.component.css',
 })
 export class ViewSnippetComponent {
+  constructor(
+    private dbService: DatabaseService,
+    private route: ActivatedRoute
+  ) {}
 
+  codeSnippet = { title: '', code: '' };
+
+  ngOnInit() {
+    const snippetId = this.route.snapshot.paramMap.get('id'); //this is to get the parameter id which sent through routes earlier using app routes ts file
+    this.dbService.getSnippetById(snippetId!).then((data: any) => {
+      this.codeSnippet = data;
+      console.log(this.codeSnippet);
+    });
+  }
 }
